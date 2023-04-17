f27ce0fce1603aca92f29b412ba36d586976f5be
TaeYeong Im <imtaebari@gmail.com>
2023-04-17 14:33:14 +0900

    Update utils.go

--- src/agent/common/utils/utils.go
+++ src/agent/common/utils/utils.go
@@ -1,11 +1,20 @@
 package utils
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"post-api/src/agent/common"
+)
 
 func ResourceWithJson(w http.ResponseWriter, code int, payload interface{}) {
-
+	responseJson, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(responseJson)
 }
 
 func ResourceWithError(w http.ResponseWriter, code int, message string) {
-	ResourceWithJson(w, code, message)
+	// {"error": message}
+
+	ResourceWithJson(w, code, common.CustomError{Error: message})
 }
