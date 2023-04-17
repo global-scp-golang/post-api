de2d8ea5be3aa905c6f1a328ad860bdb68eca2e2
TaeYeong Im <imtaebari@gmail.com>
2023-04-17 14:30:45 +0900

    Update Post.go

--- src/agent/services/models/Post.go
+++ src/agent/services/models/Post.go
@@ -1,4 +1,11 @@
 package models
 
+import "time"
+
 type Post struct {
+	Id         int       `json:"id"`
+	Title      string    `json:"title"`
+	Content    string    `json:"content"`
+	CreatedDt  time.Time `json:"createdDt"`
+	modifiedDt time.Time `json:"modifiedDt"`
 }
