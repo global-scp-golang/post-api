1761b12e39b1cf51984baa553a22475c7a06308d
TaeYeong Im <imtaebari@gmail.com>
2023-04-17 14:32:21 +0900

    Update PostController.go

--- src/agent/controllers/PostController.go
+++ src/agent/controllers/PostController.go
@@ -1,23 +1,38 @@
 package controllers
 
 import (
+	"encoding/json"
+	"io"
 	"net/http"
-	"studySwagger/src/agent/common/utils"
-	"studySwagger/src/agent/database"
-	"studySwagger/src/agent/services"
+	"post-api/src/agent/common"
+	"post-api/src/agent/common/utils"
+	"post-api/src/agent/database"
+	"post-api/src/agent/services"
+	"post-api/src/agent/services/models"
 )
 
 func CreatePost(w http.ResponseWriter, r *http.Request) {
 	postService := new(services.PostService)
 	postService.DB = database.Connect().DB
 	// request -> body 추출
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var post models.Post
+	err = json.Unmarshal(body, &post)
+	if err != nil {
+		panic(err)
+	}
 
 	// servicec method 호출
-	resource, err := postService.CreatePost()
+	resource, err := postService.CreatePost(&post)
 	//
+
 	if err != nil {
-		utils.ResourceWithError(w, errorCode, err)
+		utils.ResourceWithError(w, common.StatusInternalServerError, err.Error())
 	} else {
-		utils.ResourceWithJson(w, statusCode, resource)
+		utils.ResourceWithJson(w, common.StatusOK, resource)
 	}
 }
