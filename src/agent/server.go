958ff8e71731a4e53a39545fd3c738e461b2c23e
TaeYeong Im <imtaebari@gmail.com>
2023-04-18 15:53:26 +0900

    Update server.go

--- src/agent/server.go
+++ src/agent/server.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/handlers"
 	"net/http"
 	"post-api/src/agent/databases"
+	_ "post-api/src/agent/docs"
 	"post-api/src/agent/routes"
 )
 
-
 func main() {
-	db := databases.OpenDb()
+	db := databases.OpenDB()
 	defer db.Close()
 	databases.SetDataBase(db)
 	router := routes.InitRoutes()
 
+	cors := handlers.CORS(
+		handlers.AllowedOrigins([]string{"*"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+	)
+
 	// router 서버 실행
 	fmt.Println("Server On :8000")
-	http.ListenAndServe(":8000", router)
+	http.ListenAndServe(":8000", cors(router))
 }
+
