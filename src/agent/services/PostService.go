ff63f1156974ccc698f48110d64b439637429d49
TaeYeong Im <imtaebari@gmail.com>
2023-04-18 16:00:34 +0900

    Update PostService.go

--- src/agent/services/PostService.go
+++ src/agent/services/PostService.go
@@ -9,13 +9,14 @@ import (
 )
 
 type PostService struct {
-	DB *sql.DB
+	DB          *sql.DB
+	updatePosts interface{}
 }
 
 func (s *PostService) CreatePost(post *models.Post) (int, error) {
 	q := query.CreatePostQuery()
 	//// 쿼리 수행
-	prepare, err := s.DB.Prepare(q) 
+	prepare, err := s.DB.Prepare(q)
 	if err != nil {
 		return 0, err
 	}
@@ -27,6 +28,57 @@ func (s *PostService) CreatePost(post *models.Post) (int, error) {
 	if err != nil {
 		return 0, err
 	}
+
+	return id, nil
+}
+
+func (s *PostService) FindAllPost() ([]*models.Post, error) {
+	q := query.FindAllPostQuery()
+	posts := []*models.Post{}
+	//// 쿼리 수행
+	rows, err := s.DB.Query(q)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var post models.Post
+		rows.Scan(&post.Id, &post.Title, &post.Content, &post.CreatedDt, &post.ModifiedDt)
+		posts = append(posts, &post)
+	}
+
+	return posts, nil
+}
+
+func (s *PostService) UpdatePosts(postId int, req models.PostUpdate) (int, error) {
+	updatePosts := query.UpdatePosts()
+
+	prepare, err := s.DB.Prepare(updatePosts)
+	if err != nil {
+		return 0, err
+	}
+
+	var id int
+
+	row := prepare.QueryRow(req.Title, req.Content, postId)
+	row.Scan(&id)
+
+	return id, nil
+}
+
+func (s *PostService) DeletePost(postId int) (int, error) {
+	deletePost := query.DeletePost()
+
+	prepare, err := s.DB.Prepare(deletePost)
+	if err != nil {
+		return 0, err
+	}
+
+	var id int
+
+	row := prepare.QueryRow(postId)
+	row.Scan(&id)
 
 	return id, nil
 }
