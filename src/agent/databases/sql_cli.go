c03f0e4d307a0e2b91e59a4f06978860c4d6251a
TaeYeong Im <imtaebari@gmail.com>
2023-04-18 17:20:24 +0900

    Update sql_cli.go

--- src/agent/databases/sql_cli.go
+++ src/agent/databases/sql_cli.go
@@ -21,8 +21,27 @@ func Connect() *Db {
 	return initMySQLDB
 }
 
-func OpenDb() *sql.DB {
+func OpenDB() *sql.DB {
 	setting, err := settings.LoadSetting()
+	if err != nil {
+		panic(err)
+	}
+
+	dbInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		setting.Database.Host, setting.Database.Port, setting.Database.User, setting.Database.Password, setting.Database.Dbname)
+	db, err := sql.Open("postgres", dbInfo)
+	if err != nil {
+		panic(err)
+	}
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxIdleConns(20) // DB Connection을 사용하지 않을때에도 Connection 풀에 최대 Connection 20개를 둔다
+	db.SetMaxOpenConns(10) // DB Connection을 최대 10개까지 동시 사용가능하다
+
+	return db
+}
+
+func OpenTestDB() *sql.DB {
+	setting, err := settings.LoadTestSetting()
 	if err != nil {
 		panic(err)
 	}
