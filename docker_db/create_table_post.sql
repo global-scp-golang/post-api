98de91cf056357f23a8008cdbd56dda3d9148117
TaeYeong Im <imtaebari@gmail.com>
2023-04-18 13:29:01 +0900

    Update create_table_post.sql

--- docker_db/create_table_post.sql
+++ docker_db/create_table_post.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS public."Posts"
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
     title character varying(100) COLLATE pg_catalog."default" NOT NULL,
     content character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    "createdDt" date NOT NULL,
-    "modifiedDt" date NOT NULL,
+    "createdDt" timestamp with time zone NOT NULL,
+    "modifiedDt" timestamp with time zone NOT NULL,
     CONSTRAINT "Posts_pkey" PRIMARY KEY (id)
 )
